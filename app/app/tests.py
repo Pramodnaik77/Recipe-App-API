0eda4c163d7d02e5f423a55a21e86d72c4272568
Pramod Naik <pramodnaik8887@gmail.com>
2023-06-20 21:55:09 +0530

    lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,8 +7,10 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTests(SimpleTestCase):
-    """Test the calc module."""
-    def test_add_numbers(self):
-        res = calc.add(5,6)
 
-        self.assertEqual(res,11)
+    """Test the calc module."""
+    
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
