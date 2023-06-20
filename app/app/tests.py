dabd9845ad1a2f92952fad7b53476fe4d9bfdd6e
Pramod Naik <pramodnaik8887@gmail.com>
2023-06-20 22:01:34 +0530

    Linting errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,8 +9,9 @@ from app import calc
 class CalcTests(SimpleTestCase):
 
     """Test the calc module."""
-    
+
     def test_add_numbers(self):
+        
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
